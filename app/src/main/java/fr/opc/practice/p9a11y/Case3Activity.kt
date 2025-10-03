e972a5e935df7f43ea4840c47dc7c2bfbd0bc994
MARC NAVARRO <navamarc@gmail.com>
2025-10-03 17:37:53 +0200

    Accessbility complet to case3

--- app/src/main/java/fr/opc/practice/p9a11y/Case3Activity.kt
+++ app/src/main/java/fr/opc/practice/p9a11y/Case3Activity.kt
@@ -2,6 +2,8 @@ package fr.opc.practice.p9a11y
 
 import android.content.res.ColorStateList
 import android.os.Bundle
+import android.view.accessibility.AccessibilityEvent
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import fr.opc.practice.p9a11y.databinding.ActivityCase3Binding
@@ -17,7 +19,12 @@ class Case3Activity : AppCompatActivity() {
 
         binding.pseudoEdit.doOnTextChanged { text, _, _, _ ->
             text?.length?.let { textLength ->
-                binding.validateButton.isEnabled = textLength > 2
+                val isValid = textLength > 2
+                binding.validateButton.isEnabled = isValid
+                if (isValid) {
+                    messageAccessibility(getString(R.string.username_valid, text))
+                    returnToEdit()
+                }
                 binding.pseudoEdit.backgroundTintList = if (textLength > 2) {
                     ColorStateList.valueOf(resources.getColor(R.color.green400, theme))
                 } else {
@@ -26,5 +33,22 @@ class Case3Activity : AppCompatActivity() {
             }
         }
         binding.validateButton.isEnabled = false
+
+        binding.validateButton.setOnClickListener{
+            messageAccessibility(getString(R.string.connected))
+        }
+
     }
+
+    private fun returnToEdit() {
+        binding.pseudoEdit.post {
+            binding.pseudoEdit.requestFocus()
+            binding.pseudoEdit.sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_FOCUSED)
+        }
+    }
+
+    private fun messageAccessibility(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
 }
